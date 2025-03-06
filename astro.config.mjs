bb6eea3dce33098339d622a102a40ecd4e74d861
Juaness98 <juanes98narvaez@gmail.com>
2025-03-05 20:27:03 -0500

    Fix image

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: "https://Juaness98.github.io/condupasto",
-  base: "/condupasto/",
   image: {
     domains: ["images.unsplash.com"],
   },
